6f9d373a8ea221bd645136bccdc3e9b851f009cf
Taylor Cummings <tdcummings5@gmail.com>
2022-11-21 12:49:58 -0600

    commit tree dea2b6432b3c0f9687a40f8e6c56a304ecf9e769

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+console.log("names", names)
+let newArray = []
+for (let i=0; i < names.length; i++) {
+    // `Thank you,${names[i]}, for the wonderful event gift!`
+    newArray.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+} 
+return newArray 
+}
+function countDown(n) {
+    for (let i=n; i >= 0; i--) {
+        console.log(i); 
+    }  
+}
+
+    // [
+    //     "Thank you, Taylor, for the wonderful birthday gift!",
+    //     "Thank you, Treble, for the wonderful birthday gift!",
+    //     "Thank you, Nina, for the wonderful birthday gift!",
+    // ]
+ 
